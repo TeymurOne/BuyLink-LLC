19c8bd0438dd5ed29a02c41e3cb077cd11a19b3a
Ferhad Qenberov <qenberovferhad@gmail.com>
2024-01-31 15:31:16 +0400

    asa

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  port: 3000,
   build: {
     rollupOptions: {
-      external: ['./axios'],
+      external: ['axios'],
     },
   },
 });
