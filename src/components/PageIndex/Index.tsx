c5a273a99783fac00e6c4c683db35493eb84b696
Ferhad Qenberov <qenberovferhad@gmail.com>
2024-02-10 15:03:43 +0400

    container

--- src/components/PageIndex/Index.tsx
+++ src/components/PageIndex/Index.tsx
@@ -42,14 +42,14 @@ const Index = () => {
   return (
     <>
       <div className="bg-white ">
-        <div className="bg-white max-w-6xl  mx-auto h-auto  cursor-pointer">
+        <div className="bg-white max-w-[1550px]  mx-auto h-auto  cursor-pointer">
           <Header />
           <Main />
         </div>
       </div>
 
       <div className="bg-[#F3F4F6] px-4">
-        <div className="max-w-6xl mx-auto">
+        <div className="max-w-[1550px] mx-auto">
           <Slider />
 
           <VideoSection />
@@ -60,24 +60,24 @@ const Index = () => {
       </div>
 
       <div className="bg-white  py-20">
-        <div className="max-w-6xl mx-auto">
+        <div className="max-w-[1550px] mx-auto">
           <BuyLinkPartner2 />
         </div>
       </div>
       <div className="bg-[#F3F4F6] py-20 ">
-        <div className="max-w-6xl mx-auto">
+        <div className="max-w-[1550px] mx-auto">
           <BuyLinkPartner3 />
         </div>
       </div>
 
       <div className=" bg-[#4C5DF5]   ">
-        <div className="max-w-6xl mx-auto">
+        <div className="max-w-[1550px] mx-auto">
           <Partner />
         </div>
       </div>
       <NewsLetter />
       <div className=" bg-[#F3F4F6] py-10 ">
-        <div className="max-w-[1500px] py-20 mx-auto">
+        <div className="max-w-[1550px] py-20 mx-auto">
           <Accordion />
         </div>
       </div>
