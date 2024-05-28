cff22403419f7d2be5c3914c5d2022f4d3fb0ef1
Ferhad Qenberov <qenberovferhad@gmail.com>
2024-05-28 16:04:04 +0400

    Ferhad

--- src/components/Balanc/CardDataTime.tsx
+++ src/components/Balanc/CardDataTime.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import React from 'react';
+import DateCard from './DateCard';
 
 interface CardDataTimeProps {
   children: ReactNode;
@@ -6,13 +8,10 @@ interface CardDataTimeProps {
   rate?: string | null; // rate değeri null olabilir
 }
 
-import React from 'react';
-import DateCard from './DateCard';
-
 const CardDataTime: React.FC<CardDataTimeProps> = ({ rate }) => {
   if (!rate) {
     return (
-  <DateCard desc="No date time" svg="#FF0000"/>
+      <DateCard desc="No date time" svg="#FF0000"/>
     );
   }
 
@@ -34,16 +33,15 @@ const CardDataTime: React.FC<CardDataTimeProps> = ({ rate }) => {
   const [month, day] = date.split('-');
   const monthName = months[parseInt(month) - 1];
 
-  const svgColor = day > '0' ? '#39B554' : day == '0' ? '#FF8D24' : '#FF0000';
+  const svgColor = parseInt(day) > 0 ? '#39B554' : parseInt(day) === 0 ? '#FF8D24' : '#FF0000';
   const desc =
-    day > '0'
+    parseInt(day) > 0
       ? 'In case of late payment, there will be penalties'
-      : day == '0'
+      : parseInt(day) === 0
       ? 'Penalties start to charge (0.1% of total amount)'
       : 'Penalties start to charge (0.1% of total amount)';
 
   return (
-  
     <DateCard desc={desc} day={day} month={monthName} svg={svgColor}/>
   );
 };
