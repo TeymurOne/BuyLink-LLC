b58edc71aaf3fb27dd2855ca311b38e6a37fc9ce
ShehlaKazimova <shehla.kazimova01@gmail.com>
2024-09-13 16:12:14 +0400

    FRONTEND-103 Remove the ru language from web

--- src/components/DropdownUser.tsx
+++ src/components/DropdownUser.tsx
@@ -20,7 +20,7 @@ const DropdownUser = () => {
   const handleLogout = () => {
     dispatch(logOut());
     document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-
+    localStorage.removeItem('lng');
     navigate('/');
     setTimeout(() => {
       window.location.reload();
