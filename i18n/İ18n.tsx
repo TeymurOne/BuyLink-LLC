402cd114e263060cedb1079ac03a6d8e6380f87d
ShehlaKazimova <shehla.kazimova01@gmail.com>
2024-10-09 14:01:06 +0400

    FRONTEND-119 changed the language in operation's edit info

--- i18n/İ18n.tsx
+++ i18n/İ18n.tsx
@@ -69,6 +69,7 @@ const resources = {
         '10': 'Seçin',
         '11': 'Şifrəni Dəyiş',
         '12': 'Operator Detalları',
+        '13': 'Şifrə yalnız mobil proqramda dəyişəcək.',
       },
       partnerinfo: {
         '0': 'Partnyor Məlumatları ',
@@ -521,6 +522,7 @@ const resources = {
         '10': 'Select',
         '11': 'Change Password',
         '12': 'Operator Details',
+        '13': 'Password will only change on mobile application.',
       },
       partnerinfo: {
         '0': 'Partner Info',
@@ -975,6 +977,7 @@ const resources = {
         '10': 'Выбрать',
         '11': 'Изменить пароль',
         '12': 'Детали Оператора',
+        '13': 'Пароль изменится только в мобильном приложении.',
       },
       partnerinfo: {
         '0': 'Информация о партнере',
